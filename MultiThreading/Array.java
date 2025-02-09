e31676c67f7637294f97517378a870e2c40c8a2e
bajishaikh18 <bajishaikh18@gmail.com>
2025-02-09 08:43:00 +0530

    Initial Message

--- MultiThreading/Array.java
+++ MultiThreading/Array.java
@@ -1,77 +1,77 @@
 import java.util.*;
 
 class searchTask implements Runnable {
-	private int arr[];
-	private int num;
+    private int arr[];
+    private int num;
 
-	public searchTask(int arr[], int num) {
-		this.arr = arr;
-		this.num = num;
-	}
+    public searchTask(int arr[], int num) {
+        this.arr = arr;
+        this.num = num;
+    }
 
-	@Override
-	public void run() {
-		boolean found = false;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == num) {
-				System.out.println("Found.. at index :" + i);
-				found = true;
-				break;
-			}
-		}
-		if (!found) {
-			System.out.println("Not Found..");
-		}
-	}
+    @Override
+    public void run() {
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                System.out.println("Found.. at index :" + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Not Found..");
+        }
+    }
 }
 
 class sortTask implements Runnable {
-	private int arr[];
+    private int arr[];
 
-	public sortTask(int arr[]) {
-		this.arr = arr;
-	}
+    public sortTask(int arr[]) {
+        this.arr = arr;
+    }
 
-	@Override
-	public void run() {
-		Arrays.sort(arr);
+    @Override
+    public void run() {
+        Arrays.sort(arr);
 
-		System.out.println("After Sorting" + Arrays.toString(arr));
-	}
+        System.out.println("After Sorting" + Arrays.toString(arr));
+    }
 }
 
 public class Array {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Length of the Array..");
-		int n = sc.nextInt();
-		System.out.println("Enter The Number to Search");
-		int num = sc.nextInt();
-		int arr[] = new int[n];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Length of the Array..");
+        int n = sc.nextInt();
+        System.out.println("Enter The Number to Search");
+        int num = sc.nextInt();
+        int arr[] = new int[n];
 
-		for (int i = 0; i < n; i++) {
-			System.out.println("Enter the Element " + i);
-			arr[i] = sc.nextInt();
-		}
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the Element " + i);
+            arr[i] = sc.nextInt();
+        }
 
-		Thread sortThread = new Thread(new sortTask(arr));
-		sortThread.start();
+        Thread sortThread = new Thread(new sortTask(arr));
+        sortThread.start();
 
-		try {
-			sortThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        try {
+            sortThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-		Thread searchThread = new Thread(new searchTask(arr, num));
-		searchThread.start();
+        Thread searchThread = new Thread(new searchTask(arr, num));
+        searchThread.start();
 
-		try {
-			searchThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        try {
+            searchThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-		sc.close();
-	}
+        sc.close();
+    }
 }
