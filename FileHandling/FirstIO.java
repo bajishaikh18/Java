51e9773d100ece6ee9bedb54f243430ffd647d9b
bajishaikh18 <bajishaikh18@gmail.com>
2025-02-11 23:00:42 +0530

    Initial Commit

--- FileHandling/FirstIO.java
+++ FileHandling/FirstIO.java
@@ -1,9 +1,20 @@
 package FileHandling;
-import java.io.*;
-import java.util.Scanner;
 
-public class FirstIO {
+import java.io.*;
+
+class FirstIO {
     public static void main(String[] args) {
-        
+        try {
+            FileInputStream ip = new FileInputStream("C:/Users/bajis/OneDrive/Desktop/Resume.pdf");
+            FileOutputStream op = new FileOutputStream("C:/Users/bajis/OneDrive/Desktop/WTFResume.pdf");
+
+            int byteData;
+
+            while ((byteData = ip.read()) != -1){
+                op.write(byteData);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
