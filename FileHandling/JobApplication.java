3d631b7d23b02e5e920fa44cf4a503cbd5e25e5f
bajishaikh18 <bajishaikh18@gmail.com>
2025-02-12 13:27:31 +0530

    Initial Commit

--- FileHandling/JobApplication.java
+++ FileHandling/JobApplication.java
@@ -7,19 +7,22 @@ public class JobApplication {
     public static void main(String[] args) {
         String path = "C:/Users/bajis/OneDrive/Desktop/Job.txt";
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Company Name :- ");
-        String company = sc.nextLine();
-        System.out.println("Enter the Date (E.g.. 25th Oct) :- ");
-        String date = sc.nextLine();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter Company Name :- ");
+            String company = sc.nextLine();
+            System.out.println("Enter the Date (E.g.. 25th Oct) :- ");
+            String date = sc.nextLine();
 
-        String entry = "Applied to " + company + " on " + date + "." + "\n";
+            String entry = "Applied to " + company + " on " + date + "." + "\n";
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
-            writer.write(entry);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+                writer.write(entry);
 
-            System.out.println("Application Logged..");
-        } catch (IOException e) {
-            e.printStackTrace();
+                System.out.println("Application Logged..");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
